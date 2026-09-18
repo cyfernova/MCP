bc62b3d3f5831c95489fa463c5940a1c24f17bdc
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-09-19 00:12:01 +0530

    S3-Bucket change

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -7,8 +7,3 @@ variable "state_bucket_name" {
   description = "Globally unique S3 bucket name for Terraform state."
   type        = string
 }
-
-variable "lock_table_name" {
-  description = "DynamoDB table name used for Terraform state locking."
-  type        = string
-}
